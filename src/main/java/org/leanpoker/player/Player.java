ac9ec7c61c6aa4ce48936cb1b3e8807b5fb31583
norcs <norbertcs@me.com>
2018-08-27 13:54:13 +0200

    Merge branch 'master' of https://github.com/prokajkristof/poker-player-kenyer
    # Conflicts:
    #	src/main/java/org/leanpoker/player/Player.java

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -15,11 +15,11 @@ public class Player {
         LeanPokerRequest pokerRequest = gson.fromJson(request.toString(),LeanPokerRequest.class);
         PlayerModel myPlayer = service.getMyPlayer(pokerRequest);
 
-        /*if(myPlayer.hole_cards[0].rank.equals(myPlayer.hole_cards[1].rank) || myPlayer.hole_cards[0].suit.equals(myPlayer.hole_cards[1].suit) || (service.convertCardRankToValue(myPlayer.hole_cards[0])+ service.convertCardRankToValue(myPlayer.hole_cards[0]))>=22){
-            return pokerRequest.currentBuyIn;
-        }*/
-        if(myPlayer.stack > service.biggestStack(pokerRequest)){
-            return service.biggestStack(pokerRequest);
+        if(myPlayer.hole_cards[0].rank.equals(myPlayer.hole_cards[1].rank) || myPlayer.hole_cards[0].suit.equals(myPlayer.hole_cards[1].suit) || (service.convertCardRankToValue(myPlayer.hole_cards[0])+ service.convertCardRankToValue(myPlayer.hole_cards[0]))>=22){
+            if(myPlayer.stack > service.biggestStack(pokerRequest)){
+                return service.biggestStack(pokerRequest);
+            }
+            return 0;
         }
         return pokerRequest.currentBuyIn;
         }catch (Exception e){
