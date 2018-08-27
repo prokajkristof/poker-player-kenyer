e2a2c4cb1e931901e95dfaa50b1942bdb25cd701
Tamas Kiss <sauriaster@gmail.com>
2018-08-27 14:20:47 +0200

    sout

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -17,9 +17,10 @@ public class Player {
         if(myPlayer.stack > service.biggestStack(pokerRequest)){
             return service.biggestStack(pokerRequest);
         }
-
+            System.out.println("BEFORE CARD CHECK");
             if(myPlayer.hole_cards[0].rank.equals(myPlayer.hole_cards[1].rank) || myPlayer.hole_cards[0].suit.equals(myPlayer.hole_cards[1].suit) || (service.convertCardRankToValue(myPlayer.hole_cards[0]) + service.convertCardRankToValue(myPlayer.hole_cards[1]))>=22){
-            return pokerRequest.currentBuyIn;
+                System.out.println("AFTER CARD CHECK");
+                return pokerRequest.currentBuyIn;
         }
         return pokerRequest.currentBuyIn;
         }catch (Exception e){
