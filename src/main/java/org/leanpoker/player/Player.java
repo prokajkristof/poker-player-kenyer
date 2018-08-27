85a8202255b29b18e092e4763d4dbdc95da1b650
Kenezke1 <herczeg.kenya2@gmail.com>
2018-08-27 10:27:44 +0200

    print request json

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,16 +2,16 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
-
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        System.out.println(request.getAsJsonArray());
         return 0;
     }
 
     public static void showdown(JsonElement game) {
+
     }
 }
