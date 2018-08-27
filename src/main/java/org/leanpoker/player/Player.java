aff306ecb8350516e7e4e0dc6b3659866df2295c
Tamas Kiss <sauriaster@gmail.com>
2018-08-27 14:12:03 +0200

    exception sout

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -18,11 +18,12 @@ public class Player {
             return service.biggestStack(pokerRequest);
         }
 
-            if(myPlayer.hole_cards[0].rank.equals(myPlayer.hole_cards[1].rank) || myPlayer.hole_cards[0].suit.equals(myPlayer.hole_cards[1].suit) || (service.convertCardRankToValue(myPlayer.hole_cards[0])+ service.convertCardRankToValue(myPlayer.hole_cards[1]))>=22){
+            if(myPlayer.hole_cards[0].rank.equals(myPlayer.hole_cards[1].rank) || myPlayer.hole_cards[0].suit.equals(myPlayer.hole_cards[1].suit) || (service.convertCardRankToValue(myPlayer.hole_cards[0]) + service.convertCardRankToValue(myPlayer.hole_cards[1]))>=22){
             return pokerRequest.currentBuyIn;
         }
         return pokerRequest.currentBuyIn;
         }catch (Exception e){
+            System.out.println("EXCEPTION ---------------> " + e.getMessage());
             return 0;
         }
     }
